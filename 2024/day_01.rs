7d1b450a8543065b3e36bca79b0258b1dbd8d405
Saúl Valdelvira <saul@saulv.es>
2024-12-02 09:00:27 +0100

    2024: Day 01

---
+++ 2024/day_01.rs
@@ -0,0 +1,29 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+fn main() {
+    let f = File::open("input.txt").expect("Error opening file");
+    let f = BufReader::new(f);
+
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+
+    f.lines().map_while(Result::ok).for_each(|line| {
+        let mut it = line.split_whitespace();
+        let n1: i32 = it.next().unwrap().parse().unwrap();
+        let n2: i32 = it.next().unwrap().parse().unwrap();
+        list1.push(n1);
+        list2.push(n2);
+    });
+
+    list1.sort();
+    list2.sort();
+
+    let part1: i32 = list1.iter().zip(list2.iter()).map(|n| (n.0 - n.1).abs()).sum();
+    println!("Part1: {part1}");
+
+    let part2: i32 = list1.iter().map(|&n| {
+        let count = list2.iter().filter(|&&n2| n2 == n).count();
+        n * count as i32
+    }).sum();
+    println!("Part2: {part2}");
+}
