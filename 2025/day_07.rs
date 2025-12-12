2a0916eaa33feaec93228c6838a0a80d3b0325ab
Saúl Valdelvira <saul@saulv.es>
2025-12-12 23:43:39 +0100

    Add visualization for 2025/Day 07

--- 2025/day_07.rs
+++ 2025/day_07.rs
@@ -1,12 +1,36 @@
 use std::collections::HashMap;
-
 use aoc::get_input_matrix;
 
-fn count_splits(start: usize, mut lines: Vec<Vec<u8>>) -> usize {
+fn visualize(i: usize, lines: &[Vec<u8>]) {
+    use std::io::{stdout, Write};
+    use std::process::Command;
+    #[cfg(target_os = "windows")]
+    const CLEAR: &str = "cls";
+    #[cfg(not(target_os = "windows"))]
+    const CLEAR: &str = "clear";
+
+    Command::new(CLEAR)
+        .spawn()
+        .unwrap()
+        .wait().unwrap();
+
+    let mut out = stdout().lock();
+    let chunk = i / 30;
+    let start = chunk * 30;
+    let end = (start + 30).min(lines.len());
+    for line in &lines[start..end] {
+        out.write_all(line).unwrap();
+        out.write_all(b"\n").unwrap();
+    }
+    std::thread::sleep(core::time::Duration::from_millis(300));
+}
+
+fn count_splits(start: usize, mut lines: Vec<Vec<u8>>, with_visualization: bool) -> usize {
     let mut beams = vec![start];
 
     let mut n_splits = 0;
-    for line in lines.iter_mut().skip(1) {
+    for i in 1..lines.len() {
+        let line = &mut lines[i];
         let mut new_beams = Vec::new();
         for &beam in &beams {
             if line[beam] == b'^' {
@@ -30,6 +54,9 @@ fn count_splits(start: usize, mut lines: Vec<Vec<u8>>) -> usize {
             }
         }
         beams = new_beams;
+        if with_visualization {
+            visualize(i, &lines);
+        }
     }
 
     n_splits
@@ -64,19 +91,20 @@ fn count_timelines(
     count
 }
 
-fn solve(lines: Vec<Vec<u8>>) -> (usize, usize) {
+fn solve(lines: Vec<Vec<u8>>, with_visualization: bool) -> (usize, usize) {
     let start = lines[0].iter().enumerate().find(|&(_, &c)| c == b'S').unwrap().0;
 
     let part2 = count_timelines(start, 1, &mut HashMap::new(), &lines);
     // Part1 mutates 'lines', so call it after part2
-    let part1 = count_splits(start, lines);
+    let part1 = count_splits(start, lines, with_visualization);
 
     (part1, part2)
 }
 
 fn main() {
     let input: Vec<Vec<u8>> = get_input_matrix();
-    aoc::run(|| solve(input));
+    let with_visualization = std::env::args().any(|s| s == "--visualize");
+    aoc::run(|| solve(input, with_visualization));
 }
 
 #[cfg(test)]
@@ -103,7 +131,7 @@ mod test {
 ..............."
 .lines()
 .map(|s| s.as_bytes().to_vec())
-.collect::<Vec<_>>());
+.collect::<Vec<_>>(), false);
         assert_eq!(part1, 21);
         assert_eq!(part2, 40);
     }
